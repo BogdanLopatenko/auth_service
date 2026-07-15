18622c6144ec944dbdfb1dbd140d3f1a8700140b
Kuleb9lka <winkeylinks@gmail.com>
2026-07-15 21:09:31 +0300

    removed @EnableFeignClients

--- src/main/java/com/auth_service/AuthServiceApplication.java
+++ src/main/java/com/auth_service/AuthServiceApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableDiscoveryClient
-@ConfigurationPropertiesScan
+@ConfigurationPropertiesScan(basePackages = "com.auth_service.config")
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
